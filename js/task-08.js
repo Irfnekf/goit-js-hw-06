ffe82a58c02f8cf94d47054f1ec68de5e1ee167d
Irfnekf <vladshkaraputa07@gmail.com>
2022-11-06 19:45:04 +0200

    homework

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const ref = document.querySelector(".login-form");
 
+const onFormTarget = (event) => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Всі поля повинні бути заповнені!");
+  }
+  const elems = {
+    email: email.value,
+    password: password.value,
+  };
+
+  event.currentTarget.reset();
+};
+
+ref.addEventListener("submit", onFormTarget);
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  spanEl: document.querySelector("span.color"),
+  bodyEl: document.querySelector("body"),
+  buttonEl: document.querySelector("button.change-color"),
+};
+
+refs.buttonEl.addEventListener("click", (event) => {
+  let newColor = getRandomHexColor();
+  refs.bodyEl.style.backgroundColor = newColor;
+  refs.spanEl.textContent = newColor;
+});
